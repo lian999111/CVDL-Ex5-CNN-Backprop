dbe8027f7b565f0953a8ca4ddc4d7cc408da58ec
Larry Liang <lian999111@gmail.com>
2019-11-25 00:06:30 +0100

    Add cnn.py

---
+++ cnn.py
@@ -0,0 +1,51 @@
+# %%
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as k
+import DLCVDatasets
+
+# %% Prepare CIFAR-10 dataset
+dataset_name = 'mnist'    # mnist or cifar10
+train_size = 60000
+test_size = 10000
+used_labels = list(range(0, 10))    # the labels to be loaded
+x_train, y_train, x_test, y_test, class_names = DLCVDatasets.get_dataset(dataset_name,
+                                                                         used_labels=used_labels,
+                                                                         training_size=train_size,
+                                                                         test_size=test_size)
+# Normalization
+if dataset_name == 'mnist':
+    x_train = np.reshape(x_train, x_train.shape+(1,))
+    x_test = np.reshape(x_test, x_test.shape+(1,))
+x_train, x_test = x_train / 255.0, x_test / 255.0
+input_shape = x_train.shape[1:4]
+
+# %% Define fully connected model
+fully_connected_model = k.Sequential([
+    k.layers.Conv2D(16, (3,3), padding='same', activation='relu', input_shape=input_shape),
+    k.layers.MaxPooling2D(2, 2),
+    k.layers.Flatten(),
+    k.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+fully_connected_model.summary()
+
+# %% Callback for early stopping
+class MyCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('accuracy') > 0.9):
+            print("\nReached 90% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+# %% Compile fully_connected model
+from tensorflow.keras.optimizers import Adam
+fully_connected_model.compile(optimizer=Adam(lr=0.001),
+                              loss='sparse_categorical_crossentropy',
+                              metrics=['accuracy'])
+
+
+# %% Train model
+fully_connected_model.fit(x_train, y_train, validation_data=(x_test, y_test),
+                          epochs=15, batch_size=64, verbose=2, callbacks=[MyCallback()])
+
+# %%
